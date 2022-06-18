c4b26dc68203363e11a93703d75508ed23cb5977
javier fernandez laptop <zhuneslave@gmail.com>
2022-06-18 15:56:23 -0500

    added test cards that use Page info from db

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,37 @@
 const router = require('express').Router();
-const { User, Pages} = require('../models');
+const { User, Pages } = require('../models');
 
 router.get("/", async (req, res) => {
-    res.render("homepage", {
-        loggedIn: req.session.loggedIn,
-    });
+    try {
+        const dbPageData = await Pages.findAll({
+            include: [
+                {   
+                    all: true,
+                    nested: true,
+                    model: Pages,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        const pages = dbPageData.map((gallery) =>
+            gallery.get({ plain: true })
+        );
+        res.render("homepage", {
+            pages,
+            loggedIn: req.session.loggedIn,
+            hasPages: pages.length > 0,
+            testCSS: true
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 //esto es una prueba
-router.get("/login", (req, res)=> {
-    if(req.session.loggedIn) {
+router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
         res.redirect("/");
         return;
     }
