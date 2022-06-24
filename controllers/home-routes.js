af13b15da9eb83fe33d3496b7f1aa6fc06de1835
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-06-24 18:53:46 -0500

    One more

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -491,10 +491,6 @@ router.post("/", async (req, res) => {
 // })
 // .catch(err => console.log(err));
 // })
-document.addEventListener('click', e =>{
-    const domValue = e.target.innerHTML;
-    console.log(domValue);
-})
 
 // router.delete("/:id", async (req, res) => {
 
