7788c8ee0d364fee7831c971b977761b24ad8749
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-06-23 22:00:02 -0500

    URLs working for each page.

--- config/connection2.js
+++ config/connection2.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'bruno2002',
+  password: '1234592',
   database: 'pin_it'
 
 });
