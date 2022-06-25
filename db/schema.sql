bdb4fb19221ca1026effcb7297f977e85d1e9f37
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-06-24 23:24:31 -0500

    schema okay

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,9 @@
 DROP DATABASE IF EXISTS pin_it;
-
 CREATE DATABASE pin_it;
 
 CREATE TABLE category (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    category VARCHAR(30),
+    category VARCHAR(30)
     
 );
 
@@ -13,6 +12,7 @@ CREATE TABLE pages (
     name VARCHAR(30) NOT NULL,
     url VARCHAR(1234),
     img VARCHAR(1234),
+    category_id INT NOT NULL,
     INDEX catId (category_id),
-    CONSTRAINT fk_department FOREIGN KEY (category_id) REFERENCES category(id) ON UPDATE CASCADE
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category(id) ON UPDATE CASCADE
 );
