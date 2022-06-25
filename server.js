db8d99d3da4e602520903474edecabc720e1c804
Alejandro Bahena <alejandro.bahsan@gmail.com>
2022-06-24 19:37:13 -0500

    Ready to deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // const hbs = exphbs.create({});
 const morgan = require("morgan")
-const Database = require('./dbpull');
+
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
